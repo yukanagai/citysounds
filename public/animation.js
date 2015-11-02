f6feb813febb9da465c918eab35689c75b33c8ec
Yuka Nagai <yukanagai@gmail.com>
2015-11-02 09:16:56 -0500

    pushing morning edits

--- public/animation.js
+++ public/animation.js
@@ -11,16 +11,14 @@ function animateRain() {
 	}
  
     for( i=1;i<dropsNum;i++) {
-    var dropLeft = randRange(0,1600);
-    var dropTop = randRange(-1000,1400);
- 
-    $('.rain').append('<div class="drop" id="drop'+i+'"></div>');
-    $('#drop'+i).css('left',dropLeft);
-    $('#drop'+i).css('top',dropTop);
+    	var dropLeft = randRange(0,1600);
+    	var dropTop = randRange(-1000,1400);
+    	$('.rain').append('<div class="drop" id="drop'+i+'"></div>');
+    	$('#drop'+i).css('left',dropLeft);
+    	$('#drop'+i).css('top',dropTop);
     }
 
     $('.rain').show();
-
 }
 
 function animateClear() {
